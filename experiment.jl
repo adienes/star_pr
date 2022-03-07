00fb558b8f89c5f6a547526caf824f85363dce75
adienes <51664769+adienes@users.noreply.github.com>
2022-03-06 19:03:19 -0500

    heavy progress on parameterized model

--- experiment.jl
+++ experiment.jl
@@ -4,6 +4,140 @@ include("./rules/quota_spending.jl")
 include("./rules/other_rules.jl")
 
 using DataFrames
+using Distributions
+using LinearAlgebra
+using Random
+
+struct SpatialParty{N}
+    μ::Vector{Float64} #center of multivariate gaussian
+    σ::Vector{Float64} #stdev along each axis
+
+    λ::Float64 #natural disposition
+    ω::Float64 #proportion of strategic voters
+
+    function SpatialParty{N}() where {N}
+        μ = (rand(N) + rand(N)) .- 1 #slight centrist bias on [-1, +1]
+        σ = rand(N)*0.28 #tip-to-tip about 20% utility
+
+        λ = sqrt(rand())
+        ω = rand()
+        new{N}(μ, σ, λ, ω)
+    end
+end
+
+struct MallowsParty
+    α::Vector{Int} #reference ranking
+    ϕ::Float64 #dispersion probability
+
+    λ::Float64 #natural disposition
+    ω::Float64 #proportion of strategic voters
+
+    function MallowsParty()
+        #these parameters imply each voter ranks around 10 candidates
+        #parties on average overlap by ~3 candidates
+        α = shuffle(unique(rand(1:30, 10)))
+        ϕ = (rand() + rand() + rand())/3
+        
+        λ = sqrt(rand())
+        ω = rand()
+        new(α, ϕ, λ, ω)
+    end
+end
+
+function getvoter(P::SpatialParty)
+    return (rand.([Normal(x...) for x in zip(P.μ, P.σ)]), rand(Bernoulli(P.ω)))
+end
+
+function getvoter(P::MallowsParty)
+    r::Vector{Int} = []
+    for (i,c) in enumerate(P.α)
+        denom = sum([P.ϕ^i for i in 0:(i-1)])
+        pos = rand(Categorical([P.ϕ^(i - j)/denom for j in 1:i]))
+        insert!(r, pos, c)
+    end
+    append!(r, shuffle(setdiff(1:30, r)))
+
+    return (r, rand(Bernoulli(P.ω)))
+end
+
+struct Electorate
+    hpuid::Int #unique identifier for following parameters
+
+    k::Int #number of winners
+    V::Int #number of voters
+    C::Int #number of candidates
+
+    M::Int #number of parties
+    Q::Vector{Float64} #proportions of voters sampled from party
+    L::Vector{Union{SpatialParty, MallowsParty}}
+
+    r::Float64  #population proportion of strategic voters
+    function Electorate(k, V, C; Party=SpatialParty{2})
+        hpuid = floor(Int, rand()*10^22)
+        M = rand(1:7)
+        L = [Party() for j in 1:M]
+        Q = rand(Dirichlet(ones(M)))
+        r = sum(*(x...) for x in zip(Q, [P.ω for P in L]))
+
+        new(hpuid, k, V, C, M, Q, L, r)
+    end
+end
+
+function getcands(E::Electorate)
+    if typeof(E.L[1]) <: MallowsParty
+        return [j for j in 1:30]
+    elseif typeof(E.L[1]) <: SpatialParty
+        N = typeof(E.L[1]).parameters[1]
+        return [(rand(N) + rand(N)) .- 1 for j in 1:E.C]
+    else
+        return nothing
+    end
+end
+
+function ξ(P::SpatialParty, v::Vector{Float64}, c::Vector{Float64})
+    return max(1 - P.λ*2*norm(c-v), 0)
+end
+
+function ξ(P::MallowsParty, v::Vector{Int}, c::Int)
+    rank = findfirst(j -> j == c, v)
+    isnothing(rank) && return 0
+    return P.λ^(rank-1)
+end
+
+function sampleutilities(E::Electorate)    
+    #ξ takes a set of latent voters to utilities
+
+    party_sampler = Categorical(E.Q)
+
+    voter_party::Vector{Int} = []
+    voter_strategy::Vector{Bool} = []
+    voter_latent::Vector{Vector{Float64}} = []
+    for i=1:E.V
+        party_affiliation = rand(party_sampler)
+
+        push!(voter_party, party_affiliation)
+
+        (vl, vs) = getvoter(E.L[party_affiliation])
+        push!(voter_strategy, vs)
+        push!(voter_latent, vl)
+    end
+
+    cands_latent = getcands(E)
+
+    S_incere = zeros(Float64, E.V, E.C)
+
+    for j=1:E.C, i=1:E.V
+        S_incere[i, j] = ξ(E.L[voter_party[i]], voter_latent[i], cands_latent[j])
+    end
+
+    return (S_incere, voter_strategy)
+end
+
+
+Ec = Electorate(5, 100, 15)
+
+display(sampleutilities(Ec)[1])
+@assert(false)
 
 function main()
     
@@ -54,6 +188,8 @@ function main()
     composite quality ranking
 
     or instead of 2d scatter, can do 2d heatmap! each hyperparam sample has 20 trials, use trimean
+
+    also look as amount of noise increases, alongside as amount of strategy increases
     =#
 
     # write a function that gives a random-normalized observation
@@ -69,9 +205,9 @@ function main()
     for i in 1:trials
         println()
         println("i = ", i)
-        V = 500
-        C = 20
-        k = 5
+        V = 5000
+        C = 50
+        k = 9
 
         #S = telos_linf_unichlet(V, C)
         if rand() < 0.6
@@ -94,10 +230,10 @@ function main()
         w3 = allocated_score(S, k)
         println("AS gives $(w3)")
 
-        w4 = spent_score(S, k)
+        w4 = spent_score_accumulation(S, k)
         println("SSS gives $(w4)")
 
-        w2 = equal_shares(S, k)
+        w2 = method_equal_shares(S, k)
         println("MES gives $(w2)")
 
         ckp, bw = cdf_transform(S)
@@ -106,11 +242,6 @@ function main()
         z = 1
         for w in (w1, w2, w3, w4)
             gm = get_metrics(S, w, V, C, k)
-
-            #stables[i, z] = stable_price(ckp, w, k; initial_budgets = bw)
-            mms[i, z] = maximin_support(ckp, w, k; initial_budgets = bw)
-            # dp[i, z] = gm["Dispersion Price"]
-            z += 1
         end
 
         if (committee_is_preferred(S, w2, w4))
@@ -127,16 +258,16 @@ function main()
         #     push!(g, m)
         # end
     end
-    println("PREFERRED MES TO SSS BY A MARGIN OF ", sss_v_mes)
-    println()
+    # println("PREFERRED MES TO SSS BY A MARGIN OF ", sss_v_mes)
+    # println()
 
-    display(stables)
-    println()
-    display(mms)
-    println()
+    # display(stables)
+    # println()
+    # display(mms)
+    # println()
 
-    display(mean.(eachcol(stables)))
-    println()
-    display(mean.(eachcol(mms)))
+    # display(mean.(eachcol(stables)))
+    # println()
+    # display(mean.(eachcol(mms)))
 end
 main()
--- rules/quota_spending.jl
+++ rules/quota_spending.jl
@@ -10,13 +10,13 @@
     #g) find lowest price such that all winners purchased same price
     #       note: similar to d)
 
-struct D
+struct ElectionState
     V::Int
     C::Int
     q::Float64
     ballot_weights::Vector{Float64}
-    unelected::Vector{Int}
-    S_wrk::Matrix{Float64}
+    remaining::Vector{Int}
+    T::Matrix{Float64}
 end
 
 function allocated_score(S, k)
@@ -35,23 +35,31 @@ function method_equal_shares(S, k)
     return sequential_quota(S, k, charge_equal_share!)
 end
 
+function spent_score_accumulation(S, k)
+    return sequential_quota(S, k, spend_score_accumulating!)
+end
+
+function stratified_sortition(S, k)
+    return sequential_quota(S, k, stratified_sortition!)
+end
+
 function sequential_quota(S::Array{Float64}, k, select_and_reweight!; complete=nothing)
     V,C = size(S)
     q = V/k
     ballot_weights::Vector{Float64} = ones(V)
-    D = aux(V, C, q, ballot_weights, Vector(1:C), copy(S))
+    D = ElectionState(V, C, q, ballot_weights, Vector(1:C), copy(S))
 
     winner_set::Vector{Int} = []
 
     w = Inf
-    while length(winner_set) < k && !isnothing(w)
+    while length(winner_set) < k && length(D.remaining) > 0 && !isnothing(w)
         w = select_and_reweight!(S, D)
         push!(winner_set, w)
-        filter!(x -> x != w, D.unelected)
+        filter!(x -> x != w, D.remaining)
     end
 
     @assert(length(winner_set) ≤ k)
-    return winner_set
+    return sort(winner_set)
 end
 
 function elimination_completion()
@@ -68,31 +76,40 @@ function single_transfer_votes!(S, D)
     each taking an equal fraction of the voting power. If you want traditional STV, do not allow
     tied scores.
     """
-    #vector containing the max score a voter has given to remaining candidates
-    voter_max_scores = mapslices(x -> maximum(x), S, dims=2)
+    relative_contribution = 0
+    surplus_factors = 0
+    while true
+        _S = S[:, D.remaining]
 
-    #0-1 array of where these max scores occur
-    preferred = (S .== voter_max_scores)
+        #0-1 array of where voter's current support is
+        preferred = (_S .== mapslices(x -> maximum(x), _S, dims=2))
 
-    #what fraction of a voter's remaining ballot weight should be assigned
-    relative_contribution = preferred ./ sum(preferred, dims=2)
+        #what fraction of a voter's remaining ballot weight should be assigned
+        relative_contribution = preferred ./ sum(preferred, dims=2)
 
-    #current support for all remaining candidates
-    surplus_factors = vec(D.ballot_weights'*relative_contribution) ./ D.q
+        #current support for all remaining candidates
+        surplus_factors = vec(D.ballot_weights'*relative_contribution) ./ D.q
 
+        (any(surplus_factors .≥ 1) || length(D.remaining) == 1) && break
+
+        lix = argmin(surplus_factors)
+        l = D.remaining[lix]
+        filter!(j -> j != l, D.remaining)
+    end
+    
     #There are some instantiations of STV where surplus votes can be transfered
     #recursively from winners to other winners. I do not allow that, but do
     #elect only one winner at a time.
-    w = argmax(surplus_factors)
+    wix = argmax(surplus_factors)
     for v in 1:D.V
         D.ballot_weights[v] = max(0, D.ballot_weights[v] - relative_contribution[v, wix] / surplus_factors[wix])
     end
 
-    return w
+    return D.remaining[wix]
 end
 
 function charge_equal_share!(S, D)
-    p_affordable = p -> vec(sum(min.(D.ballot_weights, S .* p), dims=1) .≥ D.q)
+    p_affordable = p -> vec(sum(min.(D.ballot_weights, S .* p), dims=1) .≥ D.q)[D.remaining]
 
     price_lowerbound = 0
     price_upperbound = 1 + 1 / minimum(S[S .> 0])
@@ -111,22 +128,23 @@ function charge_equal_share!(S, D)
 
     rho = price_upperbound
     supported = p_affordable(rho)
-    w = findfirst(x -> x > 0, supported)
+    wix = findfirst(x -> x > 0, supported)
 
     #Default to largest remainders completion
-    isnothing(w) && begin w = argmax(vec(sum(D.ballot_weights .* S, dims=1))) end
+    isnothing(wix) && begin wix = argmax(vec(sum(D.ballot_weights .* S[:, D.remaining], dims=1))) end
 
-    D.ballot_weights .= max.(0, D.ballot_weights - rho.*S[:, w])
-    return w
+    D.ballot_weights .= max.(0, D.ballot_weights - rho.*S[:, wix])
+    return D.remaining[wix]
 end
 
 function allocate_utilitarian!(S, D)
-    w = argmax(sum(D.ballot_weights .* S, dims=1))[2]
+    wix = argmax(vec(sum(D.ballot_weights .* S, dims=1))[D.remaining])
+    w = D.remaining[wix]
 
     winner_scores = S[:, w]
     score_sorted = sortperm(winner_scores, rev=true)
 
-    edge_voter = findfirst(r -> r ≥ q, cumsum(D.ballot_weights[score_sorted]))
+    edge_voter = findfirst(r -> r ≥ D.q, cumsum(D.ballot_weights[score_sorted]))
     if isnothing(edge_voter)
         edge_score = 0
     else
@@ -134,25 +152,28 @@ function allocate_utilitarian!(S, D)
     end
 
     voters_above_edge = (winner_scores .> edge_score)
-    ballot_weights[voters_above_edge] .== 0
+    D.ballot_weights[voters_above_edge] .== 0
 
     if edge_score > 0
         voters_on_edge = (winner_scores .== edge_score)
-        weight_above_edge = sum(ballot_weights[voters_above_edge])
-        weight_on_edge = sum(ballot_weights[voters_on_edge])
+        weight_above_edge = sum(D.ballot_weights[voters_above_edge])
+        weight_on_edge = sum(D.ballot_weights[voters_on_edge])
         surplus_factor = weight_on_edge / (D.q - weight_above_edge)
         D.ballot_weights[voters_on_edge] ./= surplus_factor
     else
         #Exhaustiveness---default completion is largest remainders
         #there may be other options though...  
     end
+
     return w
 end
 
 function spend_score_scaling!(S, D)
-    weighted_scores = S .* D.ballot_weights
+    weighted_scores = D.ballot_weights .* S
     supports = vec(sum(weighted_scores, dims=1))
-    w = argmax(supports)
+
+    wix = argmax(supports[D.remaining])
+    w = D.remaining[wix]
 
     surplus_factor = max(supports[w] / D.q, 1)
     D.ballot_weights = max.(0, (D.ballot_weights - weighted_scores[:, w] / surplus_factor))
@@ -161,26 +182,33 @@ function spend_score_scaling!(S, D)
 end
 
 function spend_score_accumulating!(S, D)
-    supports = vec(sum(D.S_wrk, dims=1))
-    w = argmax(supports)
+    supports = vec(sum(D.T, dims=1))
+    wix = argmax(supports[D.remaining])
+    w = D.remaining[wix]
 
     surplus_factor = max(supports[w] / D.q, 1)
-    score_spent = D.S_wrk[:, w] ./ surplus_factor
+    score_spent = D.T[:, w] ./ surplus_factor
 
     for i in 1:D.V
-        voter_score_to_winner = S[i, w]
-        voter_score_spent = score_spent[i]
-        for j in 1:C
-            voter_score_to_cand = S[]
+        D.T[i, S[i, :] .≥ S[i, w]] .-= score_spent[i]
+
+        running_min = 1
+        for j in sortperm(S[i, :], rev=true)
+            M = min(running_min, D.T[i, j])
+            D.T[i, j] = M
+            running_min = M
         end
     end
+
+    return w
 end
 
 function stratified_sortition!(S, D)
     dictator = rand((1:D.V)[D.ballot_weights .!= 0])
 
-    d_ballot = S[dictator, :]
-    w = rand(findall(d_ballot .== maximum(d_ballot)))
+    d_ballot = S[dictator, D.remaining]
+    wix = rand(findall(d_ballot .== maximum(d_ballot)))
+    w = D.remaining[wix]
 
     num_unexhausted = sum(D.ballot_weights)
     for i in 1:D.V
